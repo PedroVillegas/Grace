12bec6185bb9152b56a27a5805ca4e694a94c20a
Pedro Villegas <pedroarguinarena03@gmail.com>
2026-01-26 22:35:39 +0000

    Fix CMake directory issue

--- CMake/ShaderCompilation.cmake
+++ CMake/ShaderCompilation.cmake
@@ -1,37 +1,37 @@
-include(${CMAKE_SOURCE_DIR}/CMake/ValidateShaderCompiler.cmake)
+include(${PROJECT_SOURCE_DIR}/CMake/ValidateShaderCompiler.cmake)
 
 if (NOT DEFINED GRACE_CUSTOM_GLSL_COMPILER)
     find_program(GLSL_COMPILER glslc HINTS Vulkan_BIN_DIR)
     if (EXISTS ${GLSL_COMPILER})
         message(STATUS "[GRACE_INFO] Found GLSL to SPIRV compiler (${GLSL_COMPILER})")
-    endif()
-else()
+    endif ()
+else ()
     set(GLSL_COMPILER ${GRACE_CUSTOM_GLSL_COMPILER})
     message(STATUS "[GRACE_INFO] Detected path to custom GLSL compiler (${GLSL_COMPILER})")
     ValidateShaderCompiler(${GLSL_COMPILER})
-endif()
+endif ()
 
 if (NOT DEFINED GRACE_CUSTOM_HLSL_COMPILER)
     find_program(HLSL_COMPILER dxc HINTS Vulkan_BIN_DIR)
     if (EXISTS ${HLSL_COMPILER})
         message(STATUS "[GRACE_INFO] Found HLSL to SPIRV compiler (${HLSL_COMPILER})")
-    endif()
-else()
+    endif ()
+else ()
     set(HLSL_COMPILER ${GRACE_CUSTOM_HLSL_COMPILER})
     message(STATUS "[GRACE_INFO] Detected path to custom HLSL compiler (${HLSL_COMPILER})")
     ValidateShaderCompiler(${HLSL_COMPILER})
-endif()
+endif ()
 
 if (NOT DEFINED GRACE_CUSTOM_SLANG_COMPILER)
     find_program(SLANG_COMPILER slangc HINTS Vulkan_BIN_DIR)
     if (EXISTS ${SLANG_COMPILER})
         message(STATUS "[GRACE_INFO] Found SLANG to SPIRV compiler (${SLANG_COMPILER})")
-    endif()
-else()
+    endif ()
+else ()
     set(SLANG_COMPILER ${GRACE_CUSTOM_SLANG_COMPILER})
     message(STATUS "[GRACE_INFO] Detected path to custom SLANG compiler (${SLANG_COMPILER})")
     ValidateShaderCompiler(${SLANG_COMPILER})
-endif()
+endif ()
 
 if (NOT DEFINED GRACE_GLSL_SHADERS)
     message(STATUS "[GRACE_INFO] GRACE_GLSL_SHADERS not defined. Auto-searching for glsl shaders...")
@@ -40,21 +40,21 @@ if (NOT DEFINED GRACE_GLSL_SHADERS)
         "${GRACE_SHADERS_DIR}*.vert"
         "${GRACE_SHADERS_DIR}*.comp"
     )
-endif()
+endif ()
 
 if (NOT DEFINED GRACE_HLSL_SHADERS)
     message(STATUS "[GRACE_INFO] GRACE_HLSL_SHADERS not defined. Auto-searching for hlsl shaders...")
     file(GLOB_RECURSE GRACE_HLSL_SHADERS
         "${GRACE_SHADERS_DIR}*.hlsl"
     )
-endif()
+endif ()
 
 if (NOT DEFINED GRACE_SLANG_SHADERS)
     message(STATUS "[GRACE_INFO] GRACE_SLANG_SHADERS not defined. Auto-searching for slang shaders...")
     file(GLOB_RECURSE GRACE_SLANG_SHADERS
         "${GRACE_SHADERS_DIR}*.slang"
     )
-endif()
+endif ()
 
 # Note for PY_SHADER_COMPILATION_CMD:
 # The preceding space before the extra compiler args are required to prevent
